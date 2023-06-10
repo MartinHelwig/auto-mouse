56371e8bc959d73f2893278671542367ee619517
Martin Helwig <115872657+MartinHelwig@users.noreply.github.com>
2023-06-11 00:35:33 +0200

    switching to JPopupMenu and JMenuItem

--- src/main/java/io/github/martinhelwig/utility/automouse/AutoMouseTrayIcon.java
+++ src/main/java/io/github/martinhelwig/utility/automouse/AutoMouseTrayIcon.java
@@ -6,17 +6,19 @@ package io.github.martinhelwig.utility.automouse;
 
 import java.awt.AWTException;
 import java.awt.Image;
-import java.awt.MenuItem;
-import java.awt.PopupMenu;
 import java.awt.SystemTray;
 import java.awt.Toolkit;
 import java.awt.TrayIcon;
 import java.awt.TrayIcon.MessageType;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ResourceBundle;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -53,27 +55,48 @@ public class AutoMouseTrayIcon extends AutoMouseCommon implements AutoMouseInter
 		Image trayImage = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("images/auto-mouse_white.png"));
 		Icon icon = new ImageIcon(new ImageIcon(trayImage).getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT));
 
-		PopupMenu popupMenu = new PopupMenu();
+		JPopupMenu popupMenu = new JPopupMenu();
 
-		MenuItem menuItemConfig = new MenuItem(resourceBundle.getString("popupMenu.config"));
+		JMenuItem menuItemConfig = new JMenuItem(resourceBundle.getString("popupMenu.config"));
 		menuItemConfig.addActionListener(actionListener -> {
 			new AutoMouseConfigurationPanel(this.getConfiguration()).setVisible(true);
 		});
 		popupMenu.add(menuItemConfig);
 
-		MenuItem menuItemInfo = new MenuItem(resourceBundle.getString("popupMenu.info"));
+		JMenuItem menuItemInfo = new JMenuItem(resourceBundle.getString("popupMenu.info"));
 		menuItemInfo.addActionListener(actionListener -> JOptionPane.showMessageDialog(null,
 				resourceBundle.getString("copyright"), programName,
 				JOptionPane.INFORMATION_MESSAGE, icon));
 		popupMenu.add(menuItemInfo);
 
-		MenuItem menuItemExit = new MenuItem(resourceBundle.getString("popupMenu.exit"));
+		JMenuItem menuItemExit = new JMenuItem(resourceBundle.getString("popupMenu.exit"));
 		menuItemExit.addActionListener(actionListener -> System.exit(0));
 		popupMenu.add(menuItemExit);
 
-		trayIcon = new TrayIcon(trayImage, programName, popupMenu);
+		trayIcon = new TrayIcon(trayImage, programName);
 		trayIcon.setImageAutoSize(true);
 		systemTray.add(trayIcon);
+		
+		trayIcon.addMouseListener(new MouseAdapter() {
+		    @Override
+		    public void mouseReleased(MouseEvent e) {
+		        maybeShowPopup(e);
+		    }
+
+		    @Override
+		    public void mousePressed(MouseEvent e) {
+		        maybeShowPopup(e);
+		    }
+
+		    private void maybeShowPopup(MouseEvent e) {
+		        if (e.isPopupTrigger()) {
+		        	popupMenu.setLocation(e.getX(), e.getY());
+		        	popupMenu.setInvoker(popupMenu);
+		        	popupMenu.setVisible(true);
+		        }
+		    }
+		});
+		
 		trayIcon.setToolTip(programName + " - " + resourceBundle.getString("copyright"));
 		trayIcon.displayMessage(programName, resourceBundle.getString("messages.start"), MessageType.INFO);
 	}
