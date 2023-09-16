ec0ef64347c8dae90e48762ef6c75974b783cdaf
Martin Helwig <115872657+MartinHelwig@users.noreply.github.com>
2023-09-16 13:17:38 +0200

    using integrated jre if available

--- src/main/scripts/start.cmd
+++ src/main/scripts/start.cmd
@@ -3,4 +3,15 @@ REM
 REM SPDX-License-Identifier: MIT
 
 @echo off
-start javaw -cp config;lib\* io.github.martinhelwig.utility.automouse.AutoMouse
+
+set CURRENTDIR=%CD%
+set MAINCLASS=io.github.martinhelwig.utility.automouse.AutoMouse
+
+pushd "%~dp0"
+if exist jre\bin\javaw.exe (
+   jre\bin\javaw.exe -cp config;lib\* %MAINCLASS%
+) else (
+   start javaw.exe -cp config;lib\* %MAINCLASS%
+)
+
+pushd "%CURRENTDIR%"
